e82bd3d27af87bf4c78fc18acabb2368f099f132
Jake Ascher <ascherj@gmail.com>
2019-01-09 23:10:13 -0500

    Add BlinkyDancers to page when link is clicked

--- src/dancer.js
+++ src/dancer.js
@@ -15,8 +15,6 @@ makeDancer.prototype.step = function() {
   // the basic dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
   // this.step.bind(this)
-  // debugger;
-
   setTimeout(this.step.bind(this), this.timeBetweenSteps);
 };
 
