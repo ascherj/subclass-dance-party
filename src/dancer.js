7ca9b39428266fd54cc9a58245dd8c1f889788ec
Jake Ascher <ascherj@gmail.com>
2019-01-10 14:50:38 -0500

    Clean up dancer.js and blinkyDancer.js

--- src/dancer.js
+++ src/dancer.js
@@ -4,8 +4,6 @@ var makeDancer = function(top, left, timeBetweenSteps) {
   this.$node = $('<span class="dancer"></span>');
   this.timeBetweenSteps = timeBetweenSteps;
 
-  makeDancer.prototype.step();
-
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
   this.setPosition(top, left);
